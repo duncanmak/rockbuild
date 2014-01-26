748fe35ef3cb8ab5c7949cc1bc660666afee3a07
Cody Russell <cody@jhu.edu>
2014-01-26 11:09:48 -0600

    Clean up.

--- lib/rockbuild/strategy.rb
+++ lib/rockbuild/strategy.rb
@@ -1,18 +1,5 @@
-#cwd = File.expand_path('../', __FILE__)
-#Dir["#{cwd}/strategies/**/*.rb"].each do |pkg| puts pkg; require pkg end
-
 module Rockbuild
   class Strategy
-    # def initialize(configure: nil, build: nil, install: nil)
-    #   @configure = configure
-    #   @build = build
-    #   @install = install
-    # end
-
-    # def configure(package, profile)
-    #   @configure.call(package, profile) unless @configure.nil?
-    # end
-
     def build_all(package, profile)
       ensure_dependencies(package, profile)
 
