c8b81674bf641452d0ea914df464901029c863be
Cody Russell <cody@jhu.edu>
2014-02-08 23:56:01 -0600

    Targets.

--- packages/llvm.rb
+++ packages/llvm.rb
@@ -16,10 +16,16 @@ class Llvm < Package
   def configure_flags
     case Env.host
     when :mac
-      [
+      base = [
         '--enable-optimized',
         '--enable-targets="x86 x86_64"'
       ]
+
+      if Env.profile == :mac32
+        target = [ '--build=i386-apple-darwin11.2.0' ]
+      elsif Env.profile == :mac64
+        target = [ '--build=x86_64-apple-darwin13.0.0' ]
+      end
     when :linux
     else
     end
--- packages/mono.rb
+++ packages/mono.rb
@@ -17,12 +17,20 @@ class Mono < Package
   def configure_flags
     case Env.host
     when :mac
-      [
+      base = [
         '--enable-nls=no',
         '--with-ikvm=yes',
         '--with-moonlight=no',
         '--enable-loadedllvm'
       ]
+
+      if Env.profile == :mac32
+        target = [ '--build=i386-apple-darwin11.2.0' ]
+      elsif Env.profile == :mac64
+        target = [ '--build=x86_64-apple-darwin13.0.0' ]
+      end
+
+      base + target
     when :linux
     else
     end
