ebffe3f67275b5c8fce89f2c3fda3f271af195e1
Rodrigo Kumpera <kumpera@gmail.com>
2014-02-20 19:18:14 -0500

    Add test that ensures that a dryrun doesn't create anything.

--- test/test_project.rb
+++ test/test_project.rb
@@ -1,10 +1,21 @@
 require 'minitest/autorun'
 require 'rockbuild'
 require_relative '../projects/demo_project'
+
+cwd = File.expand_path('../', __FILE__)
+Dir["#{cwd}/../packages/**/*.rb"].each { |pkg| require pkg }
+Dir["#{cwd}/../lib/rockbuild/**/*.rb"].each { |pkg| require pkg }
+
 include Rockbuild
 
 class TestProject < Minitest::Test
   def test_empty_project_has_no_components
     assert_empty Project.new.components
+  end
+
+  def test_dry_run_creates_no_dir
+    refute File.exists?(Env.build_root)
+    Env.with_settings ({dry_run: true, quiet: true }) { DemoProject.new.start }
+    refute File.exists?(Env.build_root)
   end
 end
